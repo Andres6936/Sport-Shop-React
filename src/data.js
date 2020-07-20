d1c449731848717768dd45df90538e78b07ec2a8
Joan Andrés <andres6936@live.com>
2020-07-19 21:26:00 -0500

    Generated products and orders random

--- src/data.js
+++ src/data.js
@@ -1,12 +1,12 @@
 let faker = require("faker");
-let data = [];
-let categories = ["Watersports", "Soccer", "Chess", "Running"];
-
 faker.seed(100);
+
+let categories = ["Watersports", "Soccer", "Chess", "Running"];
+let products = [];
 
 for(let i = 1; i <= 503; ++i) {
     let category = faker.helpers.randomize(categories);
-    data.push({
+    products.push({
         id: i,
         name: faker.commerce.productName(),
         category: category,
@@ -15,10 +15,41 @@ for(let i = 1; i <= 503; ++i) {
     })
 }
 
-module.exports = function () {
-    return {
-        categories: categories,
-        products: data,
-        orders: [],
+let orders = [];
+
+for (let i = 1; i <= 103; ++i) {
+    let name = faker.name.firstName();
+    let last = faker.name.lastName();
+    let order = {
+        id: i,
+        name: `${name} ${last}`,
+        email: faker.internet.email(name, last),
+        address: faker.address.streetAddress(),
+        city: faker.address.city(),
+        zip: faker.address.zipCode(),
+        country: faker.address.country(),
+        shipped: faker.random.boolean(),
+        products: [],
     }
+
+    let productCount = faker.random.number({min: 1, max: 5});
+    let productIDS = [];
+
+    while (productIDS.length < productCount) {
+        let candidateID = faker.random.number({min: 1, max: products.length});
+        if (productIDS.indexOf(candidateID) === -1) {
+            productIDS.push(candidateID);
+        }
+    }
+
+    for (let j = 0; j < productCount; j++) {
+        order.products.push({
+            quantity: faker.random.number({min: 1, max: 10}),
+            product_id: productIDS[j],
+        })
+    }
+
+    orders.push(order);
 }
+
+module.exports = () => ({categories, products, orders})
